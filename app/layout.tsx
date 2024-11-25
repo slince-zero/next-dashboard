5cf91b09efb5317e1cbff233ca3b789c9e8a443f
slince-zero <1711257704@qq.com>
2024-11-25 15:35:46 +0800

    feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { inter } from '@/app/ui/font'
+import '@/app/ui/global.css'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
